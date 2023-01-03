334a78fca7982915443d81b44e00c28743bf567f
degobiscaia <diegobiscaiadiego@gmail.com>
2023-01-03 14:50:35 -0300

    Portfólio

---
+++ script.js
@@ -0,0 +1,14 @@
+
+window.sr = ScrollReveal({reset: true});
+
+//sr.reveal('.container', {duration: 1000});
+
+sr.reveal('.container', {
+    rotate: {x: 0, y: 80, z: 0},
+    duration: 3000
+})
+
+sr.reveal('.container-fluid', {
+    rotate: {x: 0, y: 80, z: 0},
+    duration: 3000
+})
